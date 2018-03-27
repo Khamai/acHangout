7bc540c47b8cc2d90d410014f69bc83f4b95c573
Tianze Wu <tz6137006726@gmail.com>
2018-03-27 10:58:10 -0400

    fgjh

--- acHangout/src/com/amzi/servlets/PostServlet.java
+++ acHangout/src/com/amzi/servlets/PostServlet.java
@@ -18,7 +18,8 @@ public class PostServlet extends HttpServlet{
 
 	private static final long serialVersionUID = 1L;
 
-	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {  
+	public void doPost(HttpServletRequest request, HttpServletResponse response)  
+			throws ServletException, IOException {  
 
 		response.setContentType("text/html");  
 		PrintWriter out = response.getWriter();  
@@ -38,39 +39,19 @@ public class PostServlet extends HttpServlet{
 			rd.forward(request,response);
 		}
 
-<<<<<<< HEAD
-		String values[] = {username,password,"asianfood", topic,content};
-=======
 		String values[] = 
 			{
 					username,password,cat, topic,content
 			};
->>>>>>> refs/remotes/origin/Kha
 
-<<<<<<< HEAD
-		if(Post.validate(values))
-		{  
-			out.print("<p style=\"color:red; text-align:center;\">Posted</p>");  
-			RequestDispatcher rd=request.getRequestDispatcher("index.jsp");  
-			rd.forward(request,response);  
-=======
 		if(Post.validate(values)){  
 			String pagename = "display?topic=asianfoods&page=" + lastpage;  
 			response.sendRedirect(pagename);
->>>>>>> refs/remotes/origin/Kha
 		}  
-<<<<<<< HEAD
-		else
-		{  
-			out.print("<p style=\"color:red; text-align:center;\">Signup Error</p>");  
-			RequestDispatcher rd=request.getRequestDispatcher("signup.jsp");  
-			rd.include(request,response);  
-=======
 		else{  
 			String message = "Error! Failed to submit post";
 			request.setAttribute("message", message);
 			request.getRequestDispatcher("newpost.jsp").forward(request, response);
->>>>>>> refs/remotes/origin/Kha
 		}  
 		out.close();  
 	}  
