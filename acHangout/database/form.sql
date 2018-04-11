bdc4ddd11ce7a1e33b992a08a8d86214ec523ee2
Hangpond <35636736+Hangpond@users.noreply.github.com>
2018-04-11 00:57:27 -0400

    Update form.sql

--- acHangout/database/form.sql
+++ acHangout/database/form.sql
@@ -3,7 +3,7 @@
 
 /*
 Created: 2018-03-02
-Modified: 2018-04-06
+Modified: 2018-04-11
 Model: MySQL 5.7
 Database: MySQL 5.7
 */
@@ -225,11 +225,15 @@ CREATE TABLE `subcategories`
   `name` Char(80) NOT NULL,
   `description` Char(50) NOT NULL,
   `catid` Bigint NOT NULL,
+  `author` Bigint NOT NULL,
   PRIMARY KEY (`id`)
 )
 ;
 
 CREATE INDEX `IX_Relationship2` ON `subcategories` (`catid`)
+;
+
+CREATE INDEX `IX_Relationship1` ON `subcategories` (`author`)
 ;
 
 -- Create foreign keys (relationships) section ------------------------------------------------- 
@@ -290,4 +294,7 @@ ALTER TABLE `post` ADD CONSTRAINT `subpost` FOREIGN KEY (`subcatid`) REFERENCES 
 ALTER TABLE `subcategories` ADD CONSTRAINT `catsub` FOREIGN KEY (`catid`) REFERENCES `categories` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
 ;
 
+
+ALTER TABLE `subcategories` ADD CONSTRAINT `usersub` FOREIGN KEY (`author`) REFERENCES `users` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+;
 
